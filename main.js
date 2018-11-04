fe567748eabab2a0fd6a05bed3f32dea4bb44af6
Zion Perez <zionperez@gmail.com>
2018-11-04 11:08:04 +0900

    Completed computed properties section in tutorial.

--- main.js
+++ main.js
@@ -2,8 +2,48 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        image: './assets/green-socks.jpeg',
-        inventory: 0,
-        onSale: true
-    }
+        brand: 'Vue MASTERY',
+        selectedVariant: 0,
+        onSale: true,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green",
+                variantImage: './assets/green-socks.jpeg',
+                variantQuantity: 10
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue",
+                variantImage: './assets/blue-socks.jpeg',
+                variantQuantity: 0
+            }
+        ],
+        sizes: ["small", "medium", "large"],
+        cart: 0
+    },
+    methods: {
+        addToCart: function () {
+            this.cart += 1
+        },
+        updateProduct: function (index) {
+            this.selectedVariant = index
+            console.log(index)
+        },
+        removeFromCart: function () {
+            this.cart -= 1
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        }
+    }   
 })
